4d2ea48cefbab8d8796a16b9f8c455b0e9c77d89
emma <emmafarin@gmail.com>
2021-04-01 22:42:16 +0200

    parsing de la map

---
+++ parsing/cub3d.h
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efarin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/10 11:25:58 by efarin            #+#    #+#             */
+/*   Updated: 2021/02/10 11:26:01 by efarin           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <limits.h>
+# include <fcntl.h>
+
+typedef struct	s_list
+{
+	char			*content;
+	struct s_list	*next;
+}				t_list;
+
+typedef struct	s_struct
+{
+	int				lenmax; // width de la map
+	int				Rx; // Width
+	int				Ry; // Height
+	int				i;
+	int				F_R; //couleur sol
+	int				F_G;
+	int				F_B;
+	int				C_R; //couleur ciel
+	int				C_G;
+	int				C_B;
+	int				northside;
+	char			position; //orientation WNES
+	int				xplayer; //px
+	int				yplayer; //py
+	char			*pathtoNO;
+	char			*pathtoSO;
+	char			*pathtoWE;
+	char			*pathtoEA;
+	char			*pathtoS;
+	char			*args[8];
+
+}				t_struct;
+
+t_list			*ft_lstnew(void *content);
+void			ft_lstadd_back(t_list **alst, t_list *new);
+void			ft_lstclear(t_list **lst, void (*del)(void*));
+int				ft_lstsize(t_list *lst);
+t_list			*makelst(int fd, t_struct *mstruct);
+int				getlenmax(t_list **alst);
+char			**fromlsttotab(t_list **alst, t_struct *mstruct);
+char			**fillthetabwithspaces(char **tab, t_struct *mstruct);
+void			structinit(t_struct *mstruct);
+int				checkthenorth(char **tab, t_struct *mstruct);
+int				checkalltab(char **tab, t_struct *mstruct);
+int				checkspaces(char **tab, t_struct *mstruct, int i, int j);
+int				checkthemap(char **tab, t_struct *mstruct);
+char			**ismapvalid(char **arv, int arc);
+int				ft_atoi(const char *nptr);
+size_t			ft_cmpt(char const *s, char c);
+char			*ft_free(char **tab, size_t i);
+char			**ft_mem(char const *s, char c);
+char			**ft_split(char const *s, char c);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+void			checkelemR(char *line, t_struct *mstruct);
+char			*checktheid(char *line, char a, char b);
+void			checkelemF(char *line, t_struct *mstruct);
+void			checkelemC(char *line, t_struct *mstruct);
+int				checktheorder(char **tab, t_struct *mstruct);
+char			*ft_strcpy(char *dst, const char *src, size_t dstsize);
+void			ft_bzero(void *s, size_t n);
+size_t			ft_strlen(const char *s);
+static size_t	ft_check(char c, char const *set);
+static char		*ft_putzero(char *str, size_t i);
+char			*ft_strtrim(char const *s1, char const *set);
+int				shouldiskip(char *line, t_struct *mstruct, int a);
+char			*ft_strdup(const char *s);
+int				checkplayerposition(char **tab, t_struct *mstruct);
+char			*fromdoubletosimple(char **tab, t_struct *mstruct);
+int				ft_strcmp(char *s1, char *s2);
+void			maketab(t_struct *mstruct);
+int				whatstheid(char *line, t_struct *mstruct);
+int				firstpartmap(char *line, t_struct *mstruct, int y);
+
+# endif
---
+++ parsing/islinevalid.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   islinevalid.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: efarin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/18 11:21:06 by efarin            #+#    #+#             */
+/*   Updated: 2021/02/18 11:21:09 by efarin           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+int		whatstheid(char *line, t_struct *mstruct)
+{
+	int		i;
+	int		j;
+	int		result;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	result = -1;
+	while (line[i] && line[i] == ' ')
+		i++;
+	if (!(str = ft_substr(line, i, 2)))
+		return (-1);
+	while (result != 0 && j <= 7)
+	{
+		result = ft_strcmp(str, mstruct->args[j]);
+		if (result != 0)
+			j++;
+	}
+	free(str);
+	if (result != 0)
+		return (-1);
+	return (j);
+}
+
+int		firstpartmap(char *line, t_struct *mstruct, int y)
+{
+	if (y == 0 && mstruct->Rx == 0 && mstruct->Ry == 0)
+		checkelemR(line, mstruct);
+	if (y == 0 && mstruct->Rx > 0 && mstruct->Ry > 0)
+		return (1);
+	if (y == 1 && mstruct->pathtoNO == NULL
+	&& (mstruct->pathtoNO = checktheid(line, 'N', 'O')))
+		return (1);
+	if (y == 2 && mstruct->pathtoSO == NULL
+	&& (mstruct->pathtoSO = checktheid(line, 'S', 'O')))
+		return (1);
+	if (y == 3 && mstruct->pathtoWE == NULL
+	&& (mstruct->pathtoWE = checktheid(line, 'W', 'E')))
+		return (1);
+	if (y == 4 && mstruct->pathtoEA == NULL
+	&& (mstruct->pathtoEA = checktheid(line, 'E', 'A')))
+		return (1);
+	if (y == 5 && mstruct->pathtoS == NULL
+	&& (mstruct->pathtoS = checktheid(line, 'S', ' ')))
+		return (1);
+	return (-1);
+}
+
+int		secondpartmap(char *line, t_struct *mstruct, int y)
+{
+	if (y == 6 && (mstruct->F_B == -1 && mstruct->F_R == -1
+	&& mstruct->F_G == -1))
+		checkelemF(line, mstruct);
+	if (y == 6 && (mstruct->F_R >= 0 && mstruct->F_R <= 255)
+	&& (mstruct->F_G >= 0 && mstruct->F_G <= 255)
+	&& (mstruct->F_B >= 0 && mstruct->F_B <= 255))
+		return (1);
+	if (y == 7 && (mstruct->C_B == -1 && mstruct->C_R == -1
+	&& mstruct->C_G == -1))
+		checkelemC(line, mstruct);
+	if (y == 7 && (mstruct->C_R >= 0 && mstruct->C_R <= 255)
+	&& (mstruct->C_G >= 0 && mstruct->C_G <= 255)
+	&& (mstruct->C_B >= 0 && mstruct->C_B <= 255))
+		return (1);
+	return (-1);
+}
+
+int		shouldiskip(char *line, t_struct *mstruct, int a)
+{
+	static int	x = 0;
+	int			y;
+
+	if (a == 1 && (x = 0))
+		return (0);
+	if (ft_strlen(line) > 0)
+		x++;
+	if (x <= 8 && ft_strlen(line) == 0)
+		return (1);
+	if (x > 8 && ft_strlen(line) == 0)
+		return (-1);
+	if (ft_strlen(line) > 0 && x <= 8)
+	{
+		y = whatstheid(line, mstruct);
+		if (y == -1)
+			return (-1);
+		if (y >= 0 && y <= 5)
+			return (firstpartmap(line, mstruct, y));
+		if (y == 6 || y == 7)
+			return (secondpartmap(line, mstruct, y));
+	}
+	else
+		return (0);
+	return (1);
+}
